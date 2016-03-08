b5b261f2a672edc6d961189758f6a97f9ad8029c
Ankur Gupta <ankur.gupta@intel.com>
2016-03-07 22:59:07 -0600

    initial

---
+++ openstack_dashboard/static/app/core/images/images.module.js
@@ -0,0 +1,81 @@
+/**
+ * (c) Copyright 2015 Hewlett-Packard Development Company, L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+(function() {
+  'use strict';
+
+  /**
+   * @ngdoc overview
+   * @ngname horizon.app.core.images
+   *
+   * @description
+   * Provides all of the services and widgets required
+   * to support and display images related content.
+   */
+  angular
+    .module('horizon.app.core.images', ['ngRoute'])
+    .constant('horizon.app.core.images.events', events())
+    .constant('horizon.app.core.images.non_bootable_image_types', ['aki', 'ari'])
+    .config(config);
+
+  config.$inject = [
+    '$provide',
+    '$windowProvider',
+    '$routeProvider'
+  ];
+
+  /**
+   * @ngdoc value
+   * @name horizon.app.core.images.events
+   * @description a list of events for images
+   */
+  function events() {
+    return {
+      DELETE_SUCCESS: 'horizon.app.core.images.DELETE_SUCCESS',
+      VOLUME_CHANGED: 'horizon.app.core.images.VOLUME_CHANGED',
+      UPDATE_METADATA_SUCCESS: 'horizon.app.core.images.UPDATE_METADATA_SUCCESS'
+    };
+  }
+
+  /**
+   * @name horizon.app.core.images.tableRoute
+   * @name horizon.app.core.images.detailsRoute
+   * @description Routes used by this module.
+   */
+  function config($provide, $windowProvider, $routeProvider) {
+    var path = $windowProvider.$get().STATIC_URL + 'app/core/images/';
+    $provide.constant('horizon.app.core.images.basePath', path);
+    var webroot = $windowProvider.$get().WEBROOT;
+    var tableUrl = path + "table/";
+    var projectTableRoute = webroot + 'project/ngimages/';
+    var detailsUrl = path + "detail/";
+    var projectDetailsRoute = webroot + 'project/ngimages/details/';
+
+    // Share the routes as constants so that views within the images module
+    // can create links to each other.
+    $provide.constant('horizon.app.core.images.tableRoute', projectTableRoute);
+    $provide.constant('horizon.app.core.images.detailsRoute', projectDetailsRoute);
+
+    $routeProvider
+      .when(projectTableRoute, {
+        templateUrl: tableUrl + 'images-table.html'
+      })
+      .when(projectDetailsRoute + ':imageId', {
+        templateUrl: detailsUrl + 'image-detail.html'
+      });
+  }
+
+})();
